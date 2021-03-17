0fd2625f7d7d293f1a7461614a6865bedc0d73c2
Timothy <2779546+Its-its@users.noreply.github.com>
2021-03-16 20:43:37 -0700

    v1.1 Error Display

--- src/result.rs
+++ src/result.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::ExprToken;
 
 
@@ -13,6 +15,22 @@ pub enum Error {
 	UnexpectedToken(ExprToken),
 	InvalidValue(ValueError)
 }
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		use Error::*;
+
+		match self {
+			Token => write!(f, "Token Error"),
+			InputEmpty => write!(f, "Empty Input"),
+			TrailingSlash => write!(f, "Trailing Slash"),
+			MissingRightHandExpression => write!(f, "Missing Right Hand Expression"),
+			UnexpectedToken(v) => write!(f, "Unexpected Token: {:?}", v),
+			InvalidValue(v) => write!(f, "Invalid Value: {:?}", v),
+		}
+	}
+}
+
 
 impl From<ValueError> for Error {
 	fn from(err: ValueError) -> Error {
